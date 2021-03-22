b9c859e612900e7121ab653587c344da459c23d8
akanksha-singh1 <81146752+akanksha-singh1@users.noreply.github.com>
2021-03-22 15:49:19 +0530

    intial commit of math.py file

---
+++ math.py
@@ -0,0 +1,24 @@
+#addition implimatation
+def add(x,y):
+    return x+y
+#substraction implimentation
+def subtract(x,y):
+    return x-y
+#multiplication implimentation
+def multiply(x,y):
+    return x*y
+#division implimentation
+def divide(x,y):
+    if y==0:
+        return DIVIDE_BY_0_ERROR
+    else:
+        return x/y
+<<<<<<< Updated upstream
+#square implematation
+def square:
+    return x*x
+=======
+#Square implementation
+def square(x):
+    pass
+>>>>>>> Stashed changes
